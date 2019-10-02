76bfa7a0a95880a3b8de09182ef7b3b7cd603120
luis niebla <luisniebla@protonmail.com>
2019-10-01 21:05:51 -0700

    WIP - seg faulting in thread start

--- thread_test.c
+++ thread_test.c
@@ -3,4 +3,20 @@
 struct TCB_t * RunQ;
 
 void increment_by_1() {
-  struct TCB_t * 
+  struct TCB_t * local_var = NewItem();
+  local_var->payload = 0;
+  while(1 == 1) {
+    local_var->payload += 1;
+    RunQ->payload += 1;
+    printf("This is function %d th execution of function 1 with global variable value %d", local_var->payload, RunQ->payload);
+    yield();
+  }
+}
+
+int main() {
+  start_thread(increment_by_1);
+  //run();
+
+
+  return 0;
+}

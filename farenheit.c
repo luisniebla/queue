3752734c778a887a6f9bb6978926738cd98c6a0a
Luis Niebla <luisniebla@protonmail.com>
2021-01-23 21:10:16 -0700

    1.15 - Rewrite the last remaining non-functional portion of main() for temperature

--- farenheit.c
+++ farenheit.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void print_temp_table(int lower, int upper);
+
 void loop_farenheit() {
   for(float i = 0; i < 300; i += 10) {
     printf("%3f, %6.1f\n", i, 5.0 * (i - 32.0) /9.0);
@@ -30,16 +32,25 @@ int main() {
   int lower = 0;
   int upper = 300;
 
-  printf("%3s %3s", "Farenheit", "Celsius\n");
-  while (lower < upper) {
-    int celsius = 5 * (lower - 32) / 9;
-    printf("%3d %3d\n", lower, celsius);
-    lower += 10;
-  }
+  print_temp_table(lower, upper);
 
   printf("--------\n");
   floating_farenheit();
-
   printf("----loop----\n");
   loop_farenheit();
+}
+
+
+int calc_celsius(int F) {
+  return 5 * (F - 32) / 9;
+}
+
+void print_temp_table(int lower, int upper) {
+  printf("%3s %3s", "Farenheit", "celsius\n");
+  while (lower < upper) {
+    int celsius = calc_celsius(lower);
+    printf("%3d %3d\n", lower, celsius);
+    lower += 10;
+  }
+  printf("------\n");
 }

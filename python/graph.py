0678784c7afb865b8a1be1acaca1752856c1a0c8
Luis Niebla <luisniebla@protonmail.com>
2021-03-09 22:16:38 -0700

    WIP - Graphs

--- python/graph.py
+++ python/graph.py
@@ -1,60 +1,147 @@
 class Vertex:
-    def __init__(self, value):
-        self.value = value
+    __slots__ = '_element'
+
+    def __init__(self, x):
+        self._element = x
 
     def element(self):
-        return self.value
+        return self._element
+
+    def __hash__(self):
+        return hash(id(self))
+
 
 class Edge:
-    def __init__(self, u: Vertex, v: Vertex):
-        self.edge = (u, v)
-        
-    def element(self):
-        return self.edge
-    
+    slots = '_origin', '_destination', '_element'
+
+    def __init__(self, u, v, x):
+        self._origin = u
+        self._destination = v
+        self._element = x
+        self.visited = False
+
     def endpoints(self):
-        return self.edge
+        return (self._origin, self._destination)
 
     def opposite(self, v):
-        if self.edge[0] is v:
-            return self.edge[1]
-        else:
-            return self.edge[0]
- 
+        return self._destination if v is self._origin else self._origin
+
+    def element(self):
+        return self._element
+
+    def __hash__(self):
+        return hash((self._origin, self._destination))
+
+    def is_visisted(self):
+        self.visited
+
+    def visit(self):
+        self.visited = True
+
+    def __str__(self):
+        return f'{self._origin}-{self._destination}'
+
+
 class Graph:
-    def __init__(self, vertices, edges):
-        self.vertices = vertices
-        self.edges = edges
+    def __init__(self, directed=False):
+        self._outgoing = {}
+        self._incoming = {} if directed else self._outgoing
 
-    def vertex_count():
-        return len(self.vertices)
+    def is_directed(self):
+        return self._incoming is not self._outgoing
 
-    def vertices():
-        return self.vertices
+    def vertex_count(self):
+        return len(self._outgoing)
 
-    def edge_count():
-        return len(self.edges)
+    def vertices(self):
+        return self._outgoing.keys()
 
-    def edges():
-        return self.edges
+    def edge_count(self):
+        total = sum(len(self._outgoing[v]) for v in self._outgoing)
 
-class UndirectedGraph(Graph):
-    def __init__(self, vertices, edges):
-        super().__init__(vertices, edges)
+        return total if self.is_directed() else total // 2
+
+    def edges(self):
+        result = set()
+        for m in self._outgoing.values():
+            result.update(m.values())
+        return result
 
     def get_edge(self, u, v):
-        if (u,v) in self.edges or (v,u) in self.edges:
-            return (u,v)
-        return None
+        return self._outgoing[u].get(v)
 
-    def degree(self, v):
-        degree = 0
-        for vertex in self.vertices:
-            if self.get_edge(v, vertex): 
-                degree += 1
-        return degree
-            
-    
-G = UndirectedGraph((1,2,3,4,5), [(1,2), (2,3), (3,4), (4,5)])
-assert G.get_edge((2,3)) == (2,3)
-assert G.degree(2) == 2
+    def degree(self, v, outgoing=True):
+        adj = self._outgoing if outgoing else self._incoming
+        return len(adj[v])
+
+    def incident_edges(self, v, outgoing=True):
+        adj = self._outgoing if outgoing else self._incoming
+        print('adjacent', adj)
+        for edge in adj[v].values():
+            yield edge
+
+    def insert_vertex(self, x=None):
+        v = Vertex(x)
+        self._outgoing[v] = {}
+        if self.is_directed():
+            self._incoming[v] = {}
+        return v
+
+    def insert_edge(self, u, v, x=None):
+        e = Edge(u, v, x)
+        print('')
+        print('Outgoing', self._outgoing)
+        self._outgoing[u][v] = e
+        self._incoming[v][u] = e
+
+
+G = Graph(False)
+v1 = G.insert_vertex(1)
+v2 = G.insert_vertex(2)
+G.insert_edge(v1, v2)
+
+
+def depth_first_search(G, u):
+    print(G.incident_edges(u))
+    for e in G.incident_edges(u):
+        print(e.endpoints())
+
+
+depth_first_search(G, 4)
+
+
+# class UndirectedGraph(Graph):
+#     def __init__(self, vertices, edges):
+#         super().__init__(vertices, edges)
+
+#     def get_edge(self, u, v):
+#         if (u, v) in self.edges or (v, u) in self.edges:
+#             return (u, v)
+#         return None
+
+#     def degree(self, v):
+#         degree = 0
+#         for vertex in self.vertices:
+#             if self.get_edge(v, vertex):
+#                 degree += 1
+#         return degree
+
+
+# G = UndirectedGraph((1, 2, 3, 4, 5), [(1, 2), (2, 3), (3, 4), (4, 5)])
+# assert G.get_edge((2, 3)) == (2, 3)
+# assert G.degree(2) == 2
+
+
+# def breadth_first_search(g, s, discovered):
+#     level = [s]
+#     while len(level) > 0:
+#         next_level = []
+#         for u in level:
+#             # For every outgoing edge from u
+#             for e in g.incident_edges(u):
+
+#                 v = e.opposite(u)
+#                 if v not in discovered:
+#                     discovered[v] = e
+#                     next_level.append(v)
+#         level = next_level
--- python/trie.py
+++ python/trie.py
@@ -63,14 +63,151 @@ class Trie:
         return f'{self.val}{self.left if self.left else ""}{self.right if self.right else ""}'
 
 
-obj = Trie()
-obj.insert('apple')
-# print(obj.right.right.val)
-print(obj.serailize(obj))
-print(obj)
+def inBounds(board, i, j):
+    return i >= 0 and j >= 0 and i < len(board) and j < len(board[0])
+
+
+def findWord(board, visited, i, j, word, ind, mustExist):
+    if len(word) <= ind:
+        return False
+
+    if not inBounds(board, i, j) or visited.get(f'{i}{j}'):
+        # print('visited', visited)
+        print('OUT OF BOUNDS', i, j)
+        return False
+
+    print('Testing', i, j, board[i][j], word)
+    visited[f'{i}{j}'] = True
+    if board[i][j] == word[ind]:
+        print('FOUND', word[ind])
+        if len(word) == ind + 1:
+            print('RETURNING TRUE', board[i][j], word)
+            return True
+        return (
+            findWord(board, visited, i + 1, j, word, ind + 1, True)
+            or findWord(board, visited, i - 1, j, word, ind + 1, True)
+            or findWord(board, visited, i, j + 1, word, ind + 1, True)
+            or findWord(board, visited, i, j - 1, word, ind + 1, True)
+        )
+    elif mustExist and board[i][j] != word[ind]:
+        print('FALSE')
+        return False
+    else:
+        print("NOT FOUND")
+        return (
+            findWord(board, visited, i + 1, j, word, 0, False)
+            or findWord(board, visited, i - 1, j, word, 0, False)
+            or findWord(board, visited, i, j + 1, word, 0, False)
+            or findWord(board, visited, i, j - 1, word, 0, False)
+        )
+
+
+def findWords(board, words):
+    found = []
+    for word in words:
+        print('Testing ', word)
+        if findWord(board, {}, 0, 0, word, 0, False) or findWord(
+            board, {}, 1, 0, word, 0, False
+        ):
+            found.append(word)
+    return found
+
+
+assert (
+    findWords(
+        [
+            ["o", "a", "a", "n"],
+            ["e", "t", "a", "e"],
+            ["i", "h", "k", "r"],
+            ["i", "f", "l", "v"],
+        ],
+        ["oath", "pea", "eat", "rain"],
+    )
+    == ['oath', 'eat']
+)
+
+assert findWords([["a", "a"]], ["aa"]) == ['aa']
+
+print(findWords([['a', 'a']], ['aaa']))
+# print(
+#     findWords(
+#         [
+#             ["o", "a", "b", "n"],
+#             ["o", "t", "a", "e"],
+#             ["a", "h", "k", "r"],
+#             ["a", "f", "l", "v"],
+#         ],
+#         ["oa", "oaa"],
+#     )
+# )
+
+
+# obj = Trie()
+# obj.insert('apple')
+# # print(obj.right.right.val)
+# print(obj.serailize(obj))
+# print(obj)
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+# class Solution:
+#     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+#         WORD_KEY = '$'
+
+#         trie = {}
+#         for word in words:
+#             node = trie
+#             for letter in word:
+#                 # retrieve the next node; If not found, create a empty node.
+#                 node = node.setdefault(letter, {})
+#             # mark the existence of a word in trie node
+#             node[WORD_KEY] = word
+
+#         rowNum = len(board)
+#         colNum = len(board[0])
+
+#         matchedWords = []
+
+#         def backtracking(row, col, parent):
+
+#             letter = board[row][col]
+#             currNode = parent[letter]
+
+#             # check if we find a match of word
+#             word_match = currNode.pop(WORD_KEY, False)
+#             if word_match:
+#                 # also we removed the matched word to avoid duplicates,
+#                 #   as well as avoiding using set() for results.
+#                 matchedWords.append(word_match)
+
+#             # Before the EXPLORATION, mark the cell as visited
+#             board[row][col] = '#'
+
+#             # Explore the neighbors in 4 directions, i.e. up, right, down, left
+#             for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+#                 newRow, newCol = row + rowOffset, col + colOffset
+#                 if newRow < 0 or newRow >= rowNum or newCol < 0 or newCol >= colNum:
+#                     continue
+#                 if not board[newRow][newCol] in currNode:
+#                     continue
+#                 backtracking(newRow, newCol, currNode)
+
+#             # End of EXPLORATION, we restore the cell
+#             board[row][col] = letter
+
+#             # Optimization: incrementally remove the matched leaf node in Trie.
+#             if not currNode:
+#                 parent.pop(letter)
+
+#         for row in range(rowNum):
+#             for col in range(colNum):
+#                 # starting from each of the cells
+#                 if board[row][col] in trie:
+#                     backtracking(row, col, trie)
+
+#         return matchedWords
